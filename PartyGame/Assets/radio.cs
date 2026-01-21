cf7b46185c294360bb51e35c3aa5b0e1e199300c
MUKILTEO\1717481 <1717481@mukilteo.wednet.edu>
2026-01-21 08:13:00 -0800

    musical chairs

---
+++ PartyGame/Assets/radio.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class radio : MonoBehaviour
+{
+    public List<AudioClip> music = new();
+    public AudioSource current_song;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        current_song.clip = music[Random.Range(0, music.Count)];
+        current_song.Play();
+    }
+
+    // Update is called once per frame
+}
