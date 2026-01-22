1418394cb2b8903ad10d82fd00178b868b4c7796
MUKILTEO\1725558 <jaydentran2k9@gmail.com>
2026-01-22 09:06:26 -0800

    Starting to make Tag game

---
+++ PartyGame/Assets/Jayden/UI Scripts/Game Menu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameMenu : MonoBehaviour
+{
+    private bool _isPaused;
+
+    [SerializeField] private GameObject _pauseMenuUI;
+
+    public void Paused ()
+    {
+        _isPaused = true;
+        _pauseMenuUI.SetActive(true);
+
+        Time.timeScale = 0f;
+    }
+
+    public void Resume ()
+    {
+        _isPaused = false;
+        _pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Toggle(InputAction.CallbackContext context)
+    {
+        _isPaused = !_isPaused;
+        _pauseMenuUI.SetActive(_isPaused);
+
+        Time.timeScale = _isPaused ? 0f : 1f;
+    }
+
+    public void OnApplicationQuit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+}
