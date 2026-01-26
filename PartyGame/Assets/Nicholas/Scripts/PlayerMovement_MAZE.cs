cb9099bf387ab4b44405e478fee0afcd2c28dfd1
Arcturus <systriekglitchyt@gmail.com>
2026-01-26 10:24:24 -0800

    sent player spawning to seperate script

--- PartyGame/Assets/Nicholas/Scripts/PlayerMovement_MAZE.cs
+++ PartyGame/Assets/Nicholas/Scripts/PlayerMovement_MAZE.cs
@@ -19,10 +19,6 @@ public class PlayerMovement_MAZE : MonoBehaviour
     private float sideInput;
     private float forwardInput;
 
-    // spawning
-    public SpawnPoints[] sp;
-    private Random rand = new Random();
-
     private void FixedUpdate()
     {
         if (forwardInput == Mathf.Abs(1) && sideInput == Mathf.Abs(1))
@@ -36,22 +32,6 @@ public class PlayerMovement_MAZE : MonoBehaviour
         Vector3 velocity = Vector3.ClampMagnitude(new(rb.linearVelocity.x, 0, rb.linearVelocity.z), topSpeed);
         velocity.y = rb.linearVelocity.y;
         rb.linearVelocity = velocity;
-    }
-    private void Awake()
-    {
-        bool done = false;
-        while (done == false)
-        {
-            int spPosition = rand.Next(0, sp.Length);
-            if (sp[spPosition].taken == false)
-            {
-                tf.position = sp[spPosition].tf.position;
-                sp[spPosition].taken = true;
-                done = true;
-            }
-        }
-        
-        
     }
 
     public void Move(InputAction.CallbackContext ctx)
