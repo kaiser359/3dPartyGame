30e8e0824e035f862b46521c9fa47e088525de0e
MUKILTEO\1725558 <jaydentran2k9@gmail.com>
2026-01-27 08:13:10 -0800

    Non error scripts

--- PartyGame/Assets/Jayden/Player Movement Tag/TaggerHighlight.cs
+++ PartyGame/Assets/Jayden/Player Movement Tag/TaggerHighlight.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Tagger1 : MonoBehaviour
+public class TaggerHighlight : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
