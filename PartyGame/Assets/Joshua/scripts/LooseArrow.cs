929e2d5121d7336b52444c89b1d545ac6c47855b
MUKILTEO\1726098 <Uberfrogguy@gmail.com>
2026-01-29 07:54:29 -0800

    archery multiplayer added

--- PartyGame/Assets/Joshua/scripts/LooseArrow.cs
+++ PartyGame/Assets/Joshua/scripts/LooseArrow.cs
@@ -7,13 +7,13 @@ public class LooseArrow : MonoBehaviour
     public bool loose = false;
     public BowSway BowSway;
     public bool firstStep = false;
+    public GameObject spawner;
 
     // Arrow spawn configuration
     public GameObject arrowPrefab;           
     public Transform arrowSpawnPoint;       
-    public float arrowSpeed = 30f;           
-
-    
+    public float arrowSpeed = 30f;
+    public Score Player;
 
     public void loosePrep(InputAction.CallbackContext context)
     {
@@ -29,28 +29,31 @@ public class LooseArrow : MonoBehaviour
         firstStep = true;
         loose = true;
 
-        
-
-       
         GameObject arrowInstance = Instantiate(arrowPrefab, arrowSpawnPoint.position, arrowSpawnPoint.rotation);
+        arrowInstance.GetComponent<arrow>().Player = Player;
 
+        // Record who spawned this arrow
+        LooseArrow arrowComp = arrowInstance.GetComponent<LooseArrow>();
+        if (arrowComp == null)
+        {
+            arrowComp = arrowInstance.AddComponent<LooseArrow>();
+        }
+        arrowComp.spawner = gameObject;
 
-      
         Rigidbody rb = arrowInstance.GetComponent<Rigidbody>();
         if (rb != null)
         {
+            // Use Rigidbody.velocity (not linearVelocity)
             rb.linearVelocity = arrowSpawnPoint.forward * arrowSpeed;
         }
         else
         {
-           
             rb = arrowInstance.AddComponent<Rigidbody>();
             rb.mass = 0.1f;
             rb.linearVelocity = arrowSpawnPoint.forward * arrowSpeed;
             rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
         }
 
-        
         loose = false;
         firstStep = false;
     }
