f2b3123f75500a60a1a41d5b3449b72fea933f22
MUKILTEO\1725558 <jaydentran2k9@gmail.com>
2026-02-05 08:07:00 -0800

    Timer Script added to tag game

---
+++ PartyGame/Assets/Jayden/UI Scripts/Timer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+    
+
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            //GameOver();
+            timerText.color = Color.red;
+        }
+
+
+            
+        int minutes = Mathf.FloorToInt(remainingTime / 60f);
+        int seconds = Mathf.FloorToInt(remainingTime % 60f);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
